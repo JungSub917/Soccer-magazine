c815bc0b9d1a60c5a515b9b5196a4089ba23f04c
JungSub <apple9568@naver.com>
2022-10-16 09:36:44 +0900

    make it better

--- app/server.js
+++ app/server.js
@@ -9,8 +9,8 @@ const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:false}));
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.static(path.join(__dirname, '/views')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 const users = [
   {id : 'test01', pw : '12345'},
